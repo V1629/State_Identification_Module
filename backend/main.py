aba07881d7dc1c8a3b228e3aec535ee7724cb795
V1629 <vaibhavmishraad794@gmail.com>
2026-07-03 15:05:45 +0530

    modidifed allow origins

--- backend/main.py
+++ backend/main.py
@@ -25,7 +25,7 @@ app = FastAPI(
 # CORS Middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],  # Vite dev server
+    allow_origins=["http://localhost:5173", "http://localhost:3000", "https://state-identification-frontend.onrender.com"],  # Vite dev server
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
